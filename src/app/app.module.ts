28eb199b53e51232dd9b7fa9e62fc74f65b31565
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2023-07-25 12:24:29 -0300

    Creada barra de navegación inferior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './core/components/tabs/tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TabsComponent,
   ],
   imports: [
     BrowserModule,
