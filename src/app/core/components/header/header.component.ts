e898a4578167288b80f00fe1c79bdb34d699c116
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2023-07-31 15:10:13 -0300

    Borrados console logs sueltos

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -14,15 +14,12 @@ export class HeaderComponent {
 
   esconderTitulo = effect(()=> {
     if(this.headerService.titulo()){
-      console.log("ESCONDER")
       this.claseAplicada.set("fade-out");
     }
   },{allowSignalWrites:true});
 
   mostrarTituloNuevo(e:AnimationEvent){
-    console.log(e.animationName.includes("fade-out"))
     if(e.animationName.includes("fade-out")){
-      console.log("MOSTRAR")
       this.tituloMostrado.set(this.headerService.titulo());
       this.claseAplicada.set("fade-in");
       setTimeout(()=> this.claseAplicada.set(""),250)
