d42d0637e5a95755e02fc2130ca4cf09d949bfde
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2023-08-15 18:09:34 -0300

    Página de carrito actualizada

--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -3,24 +3,52 @@ import { Component, inject } from '@angular/core';
 import { CartService } from 'src/app/core/services/cart.service';
 import { HeaderService } from 'src/app/core/services/header.service';
 import { ContadorCantidadComponent } from "../../core/components/contador-cantidad/contador-cantidad.component";
+import { Producto } from 'src/app/core/interfaces/productos';
+import { ProductosService } from 'src/app/core/services/productos.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-carrito',
     templateUrl: './carrito.component.html',
     styleUrls: ['./carrito.component.scss'],
     standalone: true,
-    imports: [CommonModule, ContadorCantidadComponent]
+    imports: [CommonModule, ContadorCantidadComponent, RouterModule]
 })
 export class CarritoComponent {
   headerService = inject(HeaderService);
   cartService = inject(CartService);
+  productosService = inject(ProductosService);
+
+  productosCarrito:Producto[]= [];
+
+  subtotal = 0;
+  delivery = 100;
+  total = 0;
 
 
   ngOnInit(): void {
     this.headerService.titulo.set("Carrito");
+    this.cartService.carrito.forEach(async itemCarrito =>{
+      const res = await this.productosService.getById(itemCarrito.idProducto)
+      if(res) this.productosCarrito.push(res);
+      this.calcularInformacion();
+    })
   }
 
   eliminarProducto(idProducto:number){
     this.cartService.eliminarProducto(idProducto);
+  }
+
+  calcularInformacion (){
+    this.subtotal = 0;
+    for (let i = 0; i < this.cartService.carrito.length; i++) {
+      this.subtotal += this.productosCarrito[i].precio * this.cartService.carrito[i].cantidad;
+    }
+    this.total = this.subtotal + this.delivery;
+  }
+
+  cambiarCantidadProducto(id:number,cantidad:number){
+    this.cartService.cambiarCantidadProducto(id,cantidad)
+    this.calcularInformacion();
   }
 }
