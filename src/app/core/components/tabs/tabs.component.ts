b38438ce674517dfac9b12ad34a2b1d58c6ac21e
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2023-09-02 16:01:53 -0300

    Cambiado sistema de marca de seleccionado en el footer

--- src/app/core/components/tabs/tabs.component.ts
+++ src/app/core/components/tabs/tabs.component.ts
@@ -8,37 +8,10 @@ import { NavigationEnd, Router } from '@angular/router';
 })
 export class TabsComponent {
 
-  constructor( private router: Router){
-    this.router.events.subscribe(event => {
-      if(event instanceof NavigationEnd){
-        switch (event.urlAfterRedirects){
-          case "/":
-            this.seleccionado = [true,false,false,false];
-            break;
-          case "/buscar":
-            this.seleccionado = [false,true,false,false];
-            break;
-          case "/carrito":
-            this.seleccionado = [false,false,true,false];
-            break;
-          case "/perfil":
-            this.seleccionado = [false,false,false,true];
-            break;
-          default:
-            this.seleccionado = [false,false,false,false];
-            break;
-        }
-      }
-    })
+  constructor(public router: Router){
   }
 
-  seleccionado = [false,false,false,false];
   colorDesactivado = "#555555";
   colorActivado = "#000000";
-
-  navegar(direccion:string){
-    //Cambiar de página
-    this.router.navigate([direccion])
-  }
 
 }
